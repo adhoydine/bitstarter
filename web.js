de0c5a6dd0c92934b8d2ba603564f49b4fceba79
Ahamadi Dhoydine <adhoydine@gmail.com>
2013-07-09 13:46:46 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,13 +1,11 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
-//app.get('/', function(request, response) {
-//  response.send('Hello World 2!');
-//});
+app.get('/', function(request, response) {
+  response.send(fs.readFileSync('index.html'));
+});
 
-var text = fs.readFileSync('index.html', 'utf8');
-console.log(text);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
